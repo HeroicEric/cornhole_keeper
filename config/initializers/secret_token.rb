3cab89d8de3e5e507d6c3eb75f60eb210281754c
Eric Kelly <heroiceric@gmail.com>
2013-05-15 19:41:36 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CornholeKeeper::Application.config.secret_token = 'd21d516e3840d39b328fa272ea9b4af3e248f31a92578493a0dc9862670c3ec36f065ff31878ba70fd53af792be5c3cd27bd0223c0168cd2e91a2fcef2209ffc'
