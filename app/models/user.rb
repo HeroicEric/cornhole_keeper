f939ec6ba88630f7c071756a9ce9047d6300abe6
Eric Kelly <heroiceric@gmail.com>
2013-05-15 21:43:17 -0400

    Remove email from users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name
 
-  validates_presence_of :email
   validates_presence_of :first_name
   validates_presence_of :last_name
 
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(:version => 20130515234724) do
   create_table "users", :force => true do |t|
     t.string   "first_name", :null => false
     t.string   "last_name",  :null => false
-    t.string   "email",      :null => false
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
